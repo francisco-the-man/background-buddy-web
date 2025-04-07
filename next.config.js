fc0f1ac09c9a2366d650b4a55b0c104d5e02db3a
francisco-the-man <avery@louisfamily.com>
2025-04-07 16:18:51 -0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  // Configure basePath if your site is not hosted at the root of the domain
-  // basePath: '/background-buddy-web',
+  basePath: '/background-buddy-web',
   images: {
     unoptimized: true,
   },
